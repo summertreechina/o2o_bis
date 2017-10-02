06e5d6a130e6d4ae21d674e3b47229525e7a4588
summetreechina <1095909657@qq.com>
2017-10-02 23:39:21 +0800

    icheck

---
+++ application/admin/controller/Category.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class category extends Controller
+{
+    public function index() {
+        return $this->fetch();
+    }
+
+    public function category() {
+    	return $this->fetch();
+    }
+
+    public function add() {
+    	return $this->fetch();
+    }
+}
