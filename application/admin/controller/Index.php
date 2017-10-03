24dc828b0779298b8c5ee686c3c152bfe4ad776c
summetreechina <1095909657@qq.com>
2017-10-03 21:24:40 +0800

    admin

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,12 +4,16 @@ use think\Controller;
 
 class Index extends Controller
 {
-    public function index() {
-    	// echo '我亲爱的nn';
-        return $this->fetch();
-    }
+	public function index() {
+		// echo '我亲爱的nn';
+		return $this->fetch();
+	}
 
-    public function test() {
-    	echo '我亲爱的nn';
-    }
+	public function test() {
+		echo '我亲爱的nn';
+	}
+
+	public function welcome() {
+		return $this->fetch();
+	}
 }
