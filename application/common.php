619bfb4004ffee290372b94bc5d3552e0755f980
summetreechina <1095909657@qq.com>
2017-10-10 17:31:14 +0800

    市北区教育局真扫兴，鄙视

--- application/common.php
+++ application/common.php
@@ -30,6 +30,39 @@ function status($status) {
 	return $str;
 }
 
+/**
+ * [获取url中的内容]
+ * @param  [type]  $url  [description]
+ * @param  integer $type [0 get, 1 post]
+ * @param  array   $data [description]
+ * @return [type]        [description]
+ */
+function doCurl($url, $type=0, $data=[]) {
+	$ch = curl_init();	// 初始化
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	if ($type == 1) {
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	}
+
+	$output = curl_exec($ch);
+
+	curl_close($ch);
+	
+	return $output;
+}
+
+
+
+
+
+
+
+
+
 
 
 
