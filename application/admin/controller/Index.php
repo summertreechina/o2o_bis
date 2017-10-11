a65eb072efed149700d10b2a2fa6e986a46db27d
summetreechina <1095909657@qq.com>
2017-10-11 19:39:07 +0800

    mapclose

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,16 +6,18 @@ class Index extends Controller
 {
 	public function index() {
 		// echo 'nn';
-		\Map::getLngLat('山东青岛市南区金湖路16号');die();
+		// \Map::getLngLat('山东青岛市南区金湖路16号');die();
 		return $this->fetch();
-	}
-
-	public function test() {
-		echo '我';
 	}
 
 	public function welcome() {
 		// return $this->fetch();
 		return '欢迎来到慕课网'.'<P>ThinkPHP-'.THINK_VERSION.'</P>';
 	}
+
+	public function map() {
+		return \Map::staticimage('山东青岛市南区金湖路16号');
+	}
+
+
 }
--- application/extra/map.php
+++ application/extra/map.php
@@ -6,6 +6,9 @@ return [
 	'ak' => '9GrctjIMfbkAh6OrythyI3s8FmsgfM0w',
 	'baidu_map_url' => 'http://api.map.baidu.com/',
 	'geocoder' => 'geocoder/v2/',
+	'width' => 400,
+	'height' => 300,
+	'staticimage' => 'staticimage/v2',
 ];
 
 
